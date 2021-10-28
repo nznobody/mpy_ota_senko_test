16cd50b9df6ed6789c1a1368ebf7e1ba3a0fe4d1
Manu Lange <Manu.Lange@plantandfood.co.nz>
2021-10-29 09:22:48 +1300

    updates to base

---
+++ boot.py
@@ -0,0 +1,4 @@
+import senko
+
+GITHUB_URL = "https://github.com/nznobody/mpy_ota_senko_test/"
+OTA = senko.Senko(url=GITHUB_URL, files=["boot.py", "main.py"])
--- main.py
+++ main.py
@@ -17,24 +17,6 @@ def wait_connected():
 def main():
     wait_connected()
 
-    tb_client = mon_thingsboard.init("KHVvf5tXJAZtZTkXlxhF")
-
-    # Main loop because digi micropython does not have threads or asyncio :(
-    next_ve_time = time.time()
-    while True:
-        curr_time = time.time()
-        if curr_time >= next_ve_time:
-            try:
-                resp = mon_vedirect.read_single_clean()
-                logging.info("Sending vedirect to Thingsboard")
-                tb_client.send_telemetry(resp)
-                logging.info("Done, will run again in ~30s")
-                next_ve_time = curr_time + 30  # Ignore time passed during operation to attempt to make periodic
-            except ValueError as _:
-                pass
-        tb_client.check_msg()
-        time.sleep_ms(100)
-
 
 if __name__ == "__main__":
     main()
